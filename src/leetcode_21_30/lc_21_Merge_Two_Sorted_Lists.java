df90ed766db5a953f06c9ef08a928b25e7704e69
cqupt_zbq <zhaoboqi77@163.com>
2022-11-30 20:18:19 +0800

    lc 21 合并两个有序链表

---
+++ src/leetcode_21_30/lc_21_Merge_Two_Sorted_Lists.java
@@ -0,0 +1,48 @@
+package leetcode_21_30;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution_21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        ListNode ans = new ListNode(-1);
+        ListNode curr = ans;
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        if(list1 != null) curr.next = list1;
+        if(list2 != null) curr.next = list2;
+        return ans.next;
+    }
+}
+
+public class lc_21_Merge_Two_Sorted_Lists {
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode res = new Solution_21().mergeTwoLists(list1, list2);
+    }
+}
